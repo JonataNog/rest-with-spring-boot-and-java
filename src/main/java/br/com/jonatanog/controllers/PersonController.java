b661632a28ab13b004decb3830496c0b71ca9c7a
Jonata Nogueira <jonatalucas2002@gmail.com>
2024-08-23 10:02:25 -0300

    Section 11 finished

--- src/main/java/br/com/jonatanog/controllers/PersonController.java
+++ src/main/java/br/com/jonatanog/controllers/PersonController.java
@@ -18,7 +18,7 @@ import br.com.jonatanog.data.vo.v1.PersonVO;
 import br.com.jonatanog.services.PersonServices;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person/v1")
 public class PersonController {
 	
 	@Autowired
---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo', 'Marina', 'Female', 'Muniz'),
+	(2, 'João Monlevade - Minas Gerais', 'Jônata', 'Male', 'Nogueira'),
+	(5, 'Nazareno - Minas Gerais', 'Bruno', 'Male', 'Eugênio'),
+	(7, 'Nazareno - Minas Gerais', 'Bruno', 'Male', 'Eugênio'),
+	(8, 'Croácia', 'Nikolas', 'Male', 'Tesla');
